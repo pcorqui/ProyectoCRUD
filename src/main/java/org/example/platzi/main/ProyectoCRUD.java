ef3250c67b967ca55d130df87dc30c13332a9ee2
Paul Cortes <pcorqui@gmail.com>
2024-05-08 22:31:43 -0600

    feature(DataBase):se prueba commit y rollback

--- src/main/java/org/example/platzi/main/ProyectoCRUD.java
+++ src/main/java/org/example/platzi/main/ProyectoCRUD.java
@@ -1,59 +1,49 @@
 package org.example.platzi.main;
 
+import org.example.platzi.model.Employee;
+import org.example.platzi.repository.EmployeeRepository;
+import org.example.platzi.repository.Repository;
 import org.example.platzi.util.DataBaseConnection;
 
+import java.net.ResponseCache;
 import java.sql.*;
 
 public class ProyectoCRUD {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
+        Connection connection = DataBaseConnection.getConnection();
 
-        PreparedStatement preparedStatement = null;
-
-        String user = "root";
-        String pass = "admin";
-        String url = "jdbc:mysql://localhost:3306/project";
-
-
-        //myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/project","root","admin");
-        try(Connection myConn = DataBaseConnection.getConnection();
-            Statement myStamt = myConn.createStatement();
-            ResultSet myrest = null;
-        ){
-            System.out.println("Genial, nos conectamos");
-
-            //myStamt = myConn.createStatement();
-//            myrest = myStamt.executeQuery("SELECT * FROM employees");
-//
-//            while(myrest.next()){
-//                System.out.println(myrest.getString("first_name"));
-//            }
-
-            String sql = ("INSERT INTO employees (first_name, pa_surname) VALUES (?,?)");
-            preparedStatement = myConn.prepareStatement(sql);
-            preparedStatement.setString(1,"Paul");
-            preparedStatement.setString(2,"Cortes");
-
-            int rowAffected = 0;//preparedStatement.executeUpdate();
-
-            if(rowAffected> 0){
-                System.out.println("Se ha creado un nuevo empleado");
-            }
-
-            //actualizar
-//            rowAffected = preparedStatement.executeUpdate("UPDATE employees set first_name='paulo' where id = 7");
-//            if(rowAffected>1){
-//                System.out.println("dato actualizado");
-//            }
-
-            //borrar
-            myStamt.executeUpdate("Delete from employees where first_name = 'Paul'");
-
-        }catch (Exception e){
-            e.printStackTrace();
-            System.out.println("Algo salio mal ):");
+        //al colocar en false el autocommit se elimina el commit automatico
+        //a la base de datos.
+        if(connection.getAutoCommit()){
+            connection.setAutoCommit(false);
         }
 
+        try{
+            Repository<Employee> repository = new EmployeeRepository(
+                    connection
+            );
 
+            repository.findAll().forEach(System.out::println);
+
+            //insertar un nuevo
+            Employee e = new Employee();
+            e.setFirst_name("paul");
+            e.setPs_surname("cortes");
+            e.setMa_surname("quijano");
+            e.setSalary(25000.0f);
+            e.setEmail("pcorquidev@gmail.com");
+            e.setCurp("abc12345890abcdefb");
+            repository.save(e);
+            connection.commit();
+
+        }catch (SQLException sqlException){
+            connection.rollback();
+            System.out.println("msj: " + sqlException.getMessage());
+            System.out.println("Algo salio mal");
+        }
+
+        //eliminar usuario
+        //repository.delete(6);
     }
 }
